723589034e67385164120a179a4a0f82893e7b68
Kris Watson <kris.watson@nanthealth.com>
2017-01-12 15:51:35 +0000

    Adding DB authentication

--- sql/MySQL/insert_data.sql
+++ sql/MySQL/insert_data.sql
@@ -20,3 +20,10 @@ INSERT INTO publication (name) VALUES ('North West / Donegal');
 INSERT INTO publication (name) VALUES ('North Coast');
 INSERT INTO publication (name) VALUES ('Mid Ulster');
 INSERT INTO publication (name) VALUES ('Belfast');
+
+-- Insert values into the role table
+INSERT INTO role (name) VALUES ('ADMIN');
+INSERT INTO role (name) VALUES ('USER');
+
+-- Create a first user
+INSERT INTO user (username, password, firstname, lastname, email, enabled, role_id) VALUES ('kris','$2a$10$RorPa6Wsf50Rk9CNSyeFpOGh/XH0iIBgCL01FkrQWyFCzYCCy58KO','Kris','Watson','treble00_01@hotmail.com',1,1);
---
+++ src/main/java/com/kricko/security/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.kricko.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.kricko.constants.Roles;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+    
+    @Autowired
+    UserDetailsService userDetailsService;
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        final String findUserQuery = "SELECT username,password,enabled "
+                + "FROM user WHERE username = ?";
+        final String findRoles = "SELECT u.username, r.name FROM user u "
+                + "JOIN role r ON u.role_id = r.id WHERE u.username =  ?";
+        
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(findUserQuery)
+                .authoritiesByUsernameQuery(findRoles);
+    }
+    
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasAnyRole(Roles.ADMIN.toString())
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login.html")
+                .permitAll();
+        http.csrf().disable();
+    }
+    
+    @Bean(name="passwordEncoder")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
