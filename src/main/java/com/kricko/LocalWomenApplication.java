a204b12cdf8ab68a8e4e6fc2d391a95141a019f7
Kris Watson <kris.watson@nanthealth.com>
2017-01-20 09:07:43 +0000

    Adding multiple properties files so that it can run on different environments

--- src/main/java/com/kricko/LocalWomenApplication.java
+++ src/main/java/com/kricko/LocalWomenApplication.java
@@ -5,9 +5,13 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ImportResource("applicationContext.xml")
+@PropertySource({ "config/application.properties",
+                  "config/dev.properties",
+                  "config/home.properties"})
 public class LocalWomenApplication {
 
     private static final Logger LOGGER = LogManager.getLogger();
