1893c8851e25dffced0f06cd2d424c9fea4826f5
kris <treble00_01@hotmail.com>
2017-02-09 19:55:04 +0000

    Update to allow to run as a jar file

--- src/main/java/com/kricko/LocalWomenApplication.java
+++ src/main/java/com/kricko/LocalWomenApplication.java
@@ -2,23 +2,16 @@ package com.kricko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ImportResource("classpath:applicationContext.xml")
-@PropertySource({ "classpath:config/application.properties",
+@PropertySource({ "classpath:config/application.yaml",
                   "classpath:config/dev.yaml",
-                  "classpath:config/home.properties"})
-public class LocalWomenApplication extends SpringBootServletInitializer {
-
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(LocalWomenApplication.class);
-     }
-
-     public static void main(String[] args) throws Exception {
+                  "classpath:config/home.yaml"})
+public class LocalWomenApplication {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(LocalWomenApplication.class, args);
-     }
+    }
 }
