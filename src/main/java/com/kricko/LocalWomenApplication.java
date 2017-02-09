c76cc713a3582dd66b0fdfbfe1f8b3d7f211ea53
Kris <Kris@Kris-PC>
2017-02-09 15:18:01 +0000

    Converting properties files to yaml files

--- src/main/java/com/kricko/LocalWomenApplication.java
+++ src/main/java/com/kricko/LocalWomenApplication.java
@@ -2,17 +2,23 @@ package com.kricko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@ImportResource("applicationContext.xml")
-@PropertySource({ "config/application.properties",
-                  "config/dev.properties",
-                  "config/home.properties"})
-public class LocalWomenApplication {
+@ImportResource("classpath:applicationContext.xml")
+@PropertySource({ "classpath:config/application.properties",
+                  "classpath:config/dev.yaml",
+                  "classpath:config/home.properties"})
+public class LocalWomenApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LocalWomenApplication.class);
+     }
+
+     public static void main(String[] args) throws Exception {
         SpringApplication.run(LocalWomenApplication.class, args);
-    }
+     }
 }
